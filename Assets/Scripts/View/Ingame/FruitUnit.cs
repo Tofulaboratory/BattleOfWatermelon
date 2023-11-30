630d5f769902fe10e70cd0582aae4de26f626e69
Tofulaboratory <tofulab1373@gmail.com>
2023-12-01 04:02:20 +0900

    フルーツの絵柄反映と当たり判定生成

--- Assets/Scripts/View/Ingame/FruitUnit.cs
+++ Assets/Scripts/View/Ingame/FruitUnit.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FruitUnit : MonoBehaviour, IFruitUnit
 {
+    [SerializeField] private FruitSpriteData fruitSpriteData;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     public void Initialize(FruitEntity entity)
     {
+        spriteRenderer.sprite = fruitSpriteData.Get(entity.Level.Value);
+        this.AddComponent<PolygonCollider2D>();
     }
 }
