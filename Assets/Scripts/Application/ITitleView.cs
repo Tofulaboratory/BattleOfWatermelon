503a67425d2f42376773ab841a2cf415a099f713
Tofulaboratory <tofulab1373@gmail.com>
2023-12-16 00:02:36 +0900

    multi start buttonの繋ぎこみ

--- Assets/Scripts/Application/ITitleView.cs
+++ Assets/Scripts/Application/ITitleView.cs
@@ -4,5 +4,6 @@ using UniRx;
 public interface ITitleView
 {
     public IObservable<Unit> OnClickStartButton();
+    public IObservable<Unit> OnClickMultiStartButton();
     public void SetActive(bool isActivate);
 }
