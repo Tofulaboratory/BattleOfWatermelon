a0ae24a9ec9514e4bb18f2a909c59b406931459d
Tofulaboratory <tofulab1373@gmail.com>
2023-12-17 01:57:55 +0900

    微修正

--- Assets/Scripts/Application/Ingame/IngamePresenter.cs
+++ Assets/Scripts/Application/Ingame/IngamePresenter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
-using TMPro;
 using UniRx;
 using Unity.VisualScripting;
 using UnityEngine;
